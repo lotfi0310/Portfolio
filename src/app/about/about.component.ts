4ae35e7c4ed7f14063738d6f061a0a2457f761bf
lotfi0310 <lotfilouiz@gmail.com>
2023-11-08 12:49:54 +0100

    update my portfolio

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,8 +9,8 @@ export class AboutComponent {
 
   downloadPdf() {
     const link = document.createElement('a');
-    link.href = 'assets/cv-lo.pdf'; // Provide the actual path to your PDF file
-    link.download = 'cv-lo.pdf'; // Define the name for the downloaded file
+    link.href = 'assets/lotfi-louiz-RSPFE2023.pdf'; // Provide the actual path to your PDF file
+    link.download = 'lotfi-louiz-RSPFE2023.pdf'; // Define the name for the downloaded file
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);  }
